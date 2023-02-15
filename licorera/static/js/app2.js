d9d8762a98a643edac51f51e0b1770e14f57c5b9
Tizmate <tizmate@gmail.com>
2023-02-15 17:49:35 -0500

    se pudo lo que se hizo

---
+++ licorera/static/js/app2.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('nav',{
+        duration: 1000,
+        origin:'bottom',
+        distance: '-50px'
+    });
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.scrolldown',{
+        duration: 2000,
+        origin:'top',
+        distance: '-500px'
+    });
+
